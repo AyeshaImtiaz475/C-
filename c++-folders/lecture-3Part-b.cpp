87d38954e1a86986bffe47838c4c869acd0ebec8
Ayesha Imtiaz <hollymusic2020@gmail.com>
2024-09-05 21:20:37 +0500

    Add files via upload

---
+++ c++-folders/lecture-3Part-b.cpp
@@ -0,0 +1,120 @@
+//loops in c++
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    //1. while loop 
+    //print number 1 to 5
+    // int count = 1;
+    // while(count <= 10)
+    // {
+    //     cout<<count <<" " ;
+    //     count++;
+    // }
+    // cout<<endl;
+
+    // int n=1;
+    // cout<<"Input the number :" <<endl;
+    // cin>>n;
+    
+    // int i=1;
+    // while(i<=n)
+    // {
+    //     cout<< i << " ";
+    //     i++;
+    // }
+    // cout<<endl;
+
+    //for loop
+    // int num = 10;
+    // for(int i=1; i<=num; i++)
+    // {
+    //     cout<<i <<" ";
+    // }
+    // cout<<endl;
+
+    //Sum of numbers from 1 to n
+    // int number;
+    // cout<<"Input the number :" <<endl;
+    // cin>>number;
+    
+    // int sum=0;
+    // for(int i=1; i<=number; i++)
+    // {
+    //     sum = sum+ i;
+    // }
+    // cout<<"Sum :" <<sum<<endl;
+
+    //same question through while loop & break , continue 
+    // int n;
+    // cout<<"Input the number:" <<endl;
+    // cin>>n;
+
+    // int i=1;
+    // int sum=0;
+    // while(i<=n)
+    // {
+    //     sum = sum+ i;
+    //     if(i == 5)
+    //     {
+    //         break;
+    //     }
+    //      i++;
+    // }
+    // cout<<"Sum :" <<sum <<endl;
+
+    //sum of all odd number 1 to N
+    // int n;
+    // cout<<"Input the number :" <<endl;
+    // cin>>n;
+    // int sum=0;
+    // for(int i =1; i<=n; i++)
+    // {
+    //      if(i%2 != 0)
+    //      {
+    //         sum = sum+i;
+    //      }
+    // }
+    // cout<<"sum is :" <<sum<<endl;
+
+    //through do while loop
+    // int n; cin>>n;
+    // int oddSum=0;
+    // int i=1;
+    // while(i<=n)
+    // {
+    //     if(i%2 != 0)
+    //     {
+    //         oddSum = oddSum+i;
+    //     }
+    //     i++;
+    // }
+    // cout<<"Odd sum:" <<oddSum<<endl;
+
+    //even sum calculation
+    // int n; cin>>n;
+    // int EvenSum=0;
+    // for(int i=1; i<=n; i++)
+    // {
+    //     if(i%2 == 0)
+    //     {
+    //         EvenSum = EvenSum + i;
+    //     }
+    // }
+    // cout<<"Even sum :" <<EvenSum<<endl;
+
+    //do-while loop 
+    do{
+        cout<<"hello world"<<endl;
+    }
+    while(3>5);
+
+
+
+   
+
+    return 0;
+}
